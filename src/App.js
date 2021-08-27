7ffa5126a18e42e3008204562535d67d8a787053
Michael-Fares <mjfares@gmail.com>
2021-08-27 11:07:20 -0500

    added true false button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+  super()
+  
+  this.state = {
+    isClicked: false
+   }
+  }
+
+  handleClick = () => {
+    // ternary operator but it can also be an if statement
+    this.state.isClicked ? 
+    this.setState({ isClicked : false }) : 
+    this.setState({ isClicked : true })
+  }
+
+  render() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={this.handleClick}>Click Me</button>
         <p>
-          Michael
+          {this.state.isClicked ? "true" : "false"}
         </p>
         <a
           className="App-link"
@@ -20,6 +39,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
